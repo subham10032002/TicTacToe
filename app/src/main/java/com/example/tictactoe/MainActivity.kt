0d97b5e4cf0fee9aa4d841309cbe569f56a16730
subham <subhamsahu039@gmail.com>
2022-02-23 02:32:50 +0530

    implement every button clicklistner

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener{
+
+    lateinit var table :  Array<Array<Button>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        table = arrayOf(
+               arrayOf(button11,button12,button13),
+               arrayOf(button21,button22,button23),
+               arrayOf(button31,button32,button33)
+        )
+
+        for(i in table)
+        {
+            for(button in i )
+            {
+                button.setOnClickListener(this)
+            }
+        }
+        button.setOnClickListener {
+
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+       when(v?.id)
+       {
+           R.id.button11 ->{
+
+           }
+           R.id.button12 ->{
+
+           }
+           R.id.button13 ->{
+
+           }
+           R.id.button21 ->{
+
+           }
+           R.id.button22 ->{
+
+           }
+           R.id.button23 ->{
+
+           }
+           R.id.button31 ->{
+
+           }
+           R.id.button32 ->{
+
+           } R.id.button33 ->{
+
+       }
+       }
     }
 }
